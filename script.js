7f1d93806551b56d60d0db8d09ea13fd0f1e6648
shweta bairwa <shwetabairwa027@gmail.com>
2023-09-12 13:00:07 +0530

    add all file

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
+  console.log("work");
   if (inputBox.value === "") {
     alert("you must write somthing!!!");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    console.log("li");
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
   inputBox.value = "";
 }
